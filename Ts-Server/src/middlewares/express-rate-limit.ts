4b3160a142397cf2939f73e5abb9b3305f6e306a
Shiv Shankar Kushwaha <119933504+ShivShankarKushwaha@users.noreply.github.com>
2025-03-11 21:39:51 +0530

    Update express-rate-limit.ts

--- Ts-Server/src/middlewares/express-rate-limit.ts
+++ Ts-Server/src/middlewares/express-rate-limit.ts
@@ -1,9 +1,9 @@
 import rateLimit from 'express-rate-limit';
 
 export const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
+	windowMs: 5 * 60 * 1000, // 5 minutes
 	max: 100, // limit each IP to 100 requests per windowMs
-	message: 'Too many requests from this IP, please try again after 15 minutes',
+	message: 'Too many requests from this IP, please try again after 5 minutes',
 	headers: true, // Send rate limit info in the headers
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false // Disable the `X-RateLimit-*` headers
